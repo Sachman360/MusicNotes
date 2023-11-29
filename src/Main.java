2a69d67df1b38d8098b60e35f09bc6288c24de04
sachal <sachalpshaikh@gmail.com>
2023-11-28 23:22:59 -0800

    Implement the music maker in main as part of the branch where it still works

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Music Notes!");
 
-        Chord c = new Chord("C#*7");
-        c.printDescription();
+        //Chord c = new Chord("C#*7");
+        //c.printDescription();
 
         MusicMaker mm = new MusicMaker();
         Song aisong = mm.generateSong();
-        //System.out.println(aisong);
+        System.out.println(aisong);
 
     }
 }
